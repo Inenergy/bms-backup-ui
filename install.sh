6d92b1526fdaf5ebcab7f0e01cd2ab29e81ccaf0
Ефимов Александр <a.efimov@inenergy.ru>
2021-10-29 11:24:03 +0300

    fix installation script

--- install.sh
+++ install.sh
@@ -1,4 +1,25 @@
+set -e
+
 npm i
 npm run build
 
-sudo systemctl enable booster.service #TODO create this service
+cat >bms-backup.service <<EOF
+[Unit]
+Description=My service
+After=network.target
+
+[Service]
+ExecStart=node server/main.js
+WorkingDirectory=/home/pi/booster-ui
+StandardOutput=inherit
+StandardError=inherit
+Restart=always
+User=pi
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo mv bms-backup.service /etc/systemd/system/
+
+sudo systemctl enable bms-backup.service #TODO create this service

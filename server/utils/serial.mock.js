8e247122923b911231b28a3e7ad368d3f72b83c6
Alexander Efimov <efimokthefatass@gmail.com>
2022-09-14 16:45:57 +0300

    make compatible with node v12

--- server/utils/serial.mock.js
+++ server/utils/serial.mock.js
@@ -13,14 +13,24 @@ for (const key in dataMap) dataMap[key] = 0;
 
 dataMap.maxBatVoltage = 100;
 dataMap.minBatVoltage = 41;
-dataMap.temp5 = 0;
-dataMap.maxFCTemp = 1;
+dataMap.minFCVoltage = 41;
+dataMap.temp1 = 21;
+dataMap.temp2 = 21;
+dataMap.temp3 = 21;
+dataMap.temp4 = 21;
+dataMap.temp5 = 21;
+dataMap.stabilizationTemp = 25;
+dataMap.maxFCTemp = 30;
+dataMap.minFCTemp = 15;
+dataMap.maxFCCurrent = 100;
 dataMap.status = {
   BMS1On: 1,
   BMS2On: 1,
   DcDcOn: 1,
   wasPurged: 1,
   wasCS: 1,
+  stabilizationMode: 1,
+  FCOn: 1,
 };
 dataMap.errors = {
   maxBatVoltage: 1,
@@ -37,7 +47,6 @@ function generateData() {
     'batVoltage',
     'fanLoadCorrective',
     'fanLoad',
-    'maxFCTemp',
   ])
     dataMap[key] = +(Math.random() * 100).toFixed(3);
   return dataMap;

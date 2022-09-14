40ffd5f6b6307a88a0fc6f318b24e435b802eac0
Alexander Efimov <efimokthefatass@gmail.com>
2022-09-14 17:17:16 +0300

    update installation instructions

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,9 @@
 set -e
-# MAIN APP INSTALLATION
+
+curl -fsSL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 npm i
 npm run build
 
-# MAIN APP AUTOSTART
-echo '/home/pi/bat-ui/dist/linux-armv7l-unpacked/booster-ui' > ~/.xinitrc
-chmod +x ~/.xinitrc
+source scripts/autostart.sh
